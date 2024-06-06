889dc50124ae4d37b81d570166d05341fa31eff1
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-06-06 11:17:59 -0300

    first commit, project configured

---
+++ src/components/home/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const Home = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+const Container = styled.section`
+    
+`
