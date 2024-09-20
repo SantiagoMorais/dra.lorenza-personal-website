80d3e7eb71d16bbd78bbfd1afa54710aba22590c
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-09-20 10:45:39 -0300

    configured article content

--- src/components/blogSection/articleList/article/index.tsx
+++ src/components/blogSection/articleList/article/index.tsx
@@ -80,22 +80,36 @@ const Container = styled.section`
     padding: 0 2rem 2rem;
 
     .postContent {
-      .postImage {
+      .postImage,
+      img {
         float: left;
-        height: 50dvh;
         min-height: 30rem;
-        max-height: 50rem;
         object-fit: cover;
         border-radius: 0.5rem;
-        border: 0.2rem solid ${style.primaryColor};
         width: 50%;
         max-width: 50rem;
         margin: 0rem 2rem 1rem 0rem;
       }
 
+      .postImage {
+        border: 0.2rem solid ${style.primaryColor};
+        box-shadow: 0.5rem 0.5rem 1rem ${style.primaryColor};
+        height: 50dvh;
+        max-height: 50rem;
+      }
+
+      img {
+        border: 0.2rem solid ${style.tertiaryColor};
+        box-shadow: -0.5rem 0.75rem 1rem ${style.tertiaryColor};
+        height: 40dvh;
+        max-height: 40rem;
+      }
+
       h2,
       h3,
-      h4 {
+      h4,
+      h5,
+      h6 {
         color: ${style.primaryColor};
         font-size: ${fontSize.fontSizeMedium};
         font-weight: ${fontStyle.mediumWeight};
@@ -126,9 +140,35 @@ const Container = styled.section`
         text-indent: 5rem;
         text-align: justify;
 
+        a {
+          color: ${style.primaryColor};
+          font-weight: ${fontStyle.mediumWeight};
+          transition: 0.3s;
+
+          &:hover {
+            opacity: 0.8;
+          }
+        }
+
         &:last-child {
           margin-bottom: 0;
         }
+      }
+
+      blockquote {
+        font-size: ${fontSize.fontSizeSmall};
+        border-left: 0.2rem solid rgba(95, 18, 18, 0.342);
+        display: inline-block;
+        color: rgba(0, 0, 0, 0.7);
+        font-weight: ${fontStyle.mediumWeight};
+        margin: 1rem 0;
+        background-color: ${style.secondaryColor};
+        border-radius: 0 1rem 1rem 0;
+        padding: .5rem 1rem;
+      }
+
+      img {
+        width: 100%;
       }
 
       .video {
@@ -144,20 +184,92 @@ const Container = styled.section`
           max-height: calc(80rem * 9 / 16);
         }
       }
+
+      video {
+        display: block;
+        border-radius: 1rem;
+        padding: 2rem 0;
+        width: 100%;
+        height: calc(60dvw * 9 / 16);
+        max-height: calc(80rem * 9 / 16);
+      }
+
+      ul,
+      ol {
+        li {
+          font-size: ${fontSize.fontSizeBase};
+          margin-left: 2rem;
+          color: ${style.secondaryTextColor};
+
+          &:last-child {
+            margin-bottom: 1rem;
+          }
+        }
+      }
+
+      ul {
+        li {
+          list-style: disc;
+        }
+      }
+
+      ol {
+        li {
+          list-style: decimal;
+        }
+      }
+
+      table {
+        width: 100%;
+        
+        thead {
+          tr {
+            th {
+              background-color: ${style.primaryColor};
+              border: .1rem solid rgba(0, 0, 0, 0.6);
+
+              p {
+                text-align: center;
+                text-indent: 0;
+              }
+            }
+          }
+        }
+        
+        tbody {
+          tr {
+            td {
+              border: .1rem solid rgba(0, 0, 0, 0.6);
+              p {
+                text-align: center;
+                text-indent: 0;
+              }
+            }
+          }
+        }
+      }
     }
   }
 
   @media (max-width: 768px) {
     .content {
       .postContent {
-        .postImage {
+        .postImage,
+        img {
           float: none;
           height: auto;
           min-height: auto;
-          max-height: 50rem;
           width: 100%;
           max-width: 100%;
           margin: 0rem 0rem 1rem;
+        }
+
+        .postImage {
+          max-height: 50rem;
+        }
+
+        img {
+          max-height: 40rem;
         }
 
         .video {
